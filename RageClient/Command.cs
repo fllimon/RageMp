27619c8fcaa069daa54b14e8715ef886036220d3
Artur Sharaenko <fallout2408@gmail.com>
2022-10-04 12:13:09 +0300

    add bind command

---
+++ RageClient/Command.cs
@@ -0,0 +1,21 @@
+﻿using RAGE;
+using RAGE.Ui;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RageClient
+{
+    internal class Command : Events.Script
+    {
+        public Command()
+        {
+            Input.Bind(VirtualKeys.P, down:true, GetCoordinate);
+        }
+
+        private void GetCoordinate()
+        {
+            Events.CallRemote("CLIENT:SERVER:GetCoordinate");
+        }
+    }
+}
---
+++ RageMpServer/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿using GTANetworkAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RageMpServer.Commands
+{
+    internal class Command : Script
+    {
+        [RemoteEvent("CLIENT:SERVER:GetCoordinate")]
+        public void GetCoordinates(Player player)
+        {
+            player.SendChatMessage($"POSITION: - X: {player.Position.X} - Z: {player.Position.X} - Z: {player.Position.Z}");
+        }
+    }
+}
